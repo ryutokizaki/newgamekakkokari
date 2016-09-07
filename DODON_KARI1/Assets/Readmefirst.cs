a2605dd47ac2771d245e13046633509f2858e8b3
ryutokizaki <ryutokizaki326@gmail.com>
2016-09-07 14:09:09 +0900

    firsttime
    HELLO

---
+++ DODON_KARI1/Assets/Readmefirst.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Readmefirst : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		/*kizakino monoDevelopsan ha nihongo yomenai node komento ha kihon iranai*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
